59e9e59e852b01a51ee3339138a01fd0c021f87e
André Tupinambá <andrelrt@gmail.com>
2018-08-09 00:08:23 +0100

    Introduce Arithmetic tests

--- samples/to_lower/to_lower.cpp
+++ samples/to_lower/to_lower.cpp
@@ -35,6 +35,32 @@
 bool g_verbose = true;
 namespace ls = litesimd;
 
+struct default_simd_to_lower
+{
+    void operator()( std::string& str )
+    {
+        ls::t_int8_simd* data = (ls::t_int8_simd*) str.data();
+
+        size_t sz = str.size() & ~(ls::int8_simd_size-1);
+        for( size_t i = 0; i < sz; i += ls::int8_simd_size)
+        {
+            *data = ls::blend< int8_t >(
+                        ls::bit_and< int8_t >(
+                            ls::greater_than( *data, int8_t('A'-1) ),
+                            ls::greater_than( int8_t('Z'+1), *data ) ),
+                        ls::add( *data, int8_t(0x20) ),
+                        *data );
+            ++data;
+        }
+
+        size_t end = str.size();
+        for( size_t i = sz; i < end; ++i )
+        {
+            str[i] = ( 'A' <= str[i] && str[i] <= 'Z' ) ? str[i] + 0x20 : str[i];
+        }
+    }
+};
+
 template< typename TAG_T >
 struct to_lower
 {
@@ -49,11 +75,11 @@ struct to_lower
         for( size_t i = 0; i < sz; i += array_size )
         {
             *data = ls::blend< int8_t, TAG_T >(
-                         ls::bit_and< int8_t, TAG_T >(
-                                 ls::greater_than< int8_t, TAG_T >( *data, 'A'-1 ),
-                                 ls::greater_than< int8_t, TAG_T >( 'Z'+1, *data ) ),
-                         ls::add< int8_t, TAG_T >( *data, 0x20 ),
-                         *data );
+                        ls::bit_and< int8_t, TAG_T >(
+                            ls::greater_than< int8_t, TAG_T >( *data, 'A'-1 ),
+                            ls::greater_than< int8_t, TAG_T >( 'Z'+1, *data ) ),
+                        ls::add< int8_t, TAG_T >( *data, 0x20 ),
+                        *data );
             ++data;
         }
 
@@ -62,7 +88,6 @@ struct to_lower
         {
             str[i] = ( 'A' <= str[i] && str[i] <= 'Z' ) ? str[i] + 0x20 : str[i];
         }
-
     }
 };
 
@@ -151,6 +176,7 @@ int main(int argc, char* /*argv*/[])
         {
             bench< std_to_lower >( "STD ...", runSize, loop );
             bench< autovec_to_lower >( "Autovec", runSize, loop );
+            bench< default_simd_to_lower >( "Default", runSize, loop );
             std::cout
                       << std::endl << "Index Speed up SSE.......: " << std::fixed << std::setprecision(2)
                       << static_cast<float>(base)/static_cast<float>(sse) << "x"
---
+++ test/arithmetic.cpp
@@ -0,0 +1,64 @@
+// MIT License
+//
+// Copyright (c) 2018 André Tupinambá
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <litesimd/types.h>
+#include <litesimd/arithmetic.h>
+#include <litesimd/compare.h>
+#include "gtest/gtest.h"
+
+namespace ls = litesimd;
+
+template <typename T> class SimdTypes: public ::testing::Test {};
+
+using TestTypes = ::testing::Types<
+#ifdef __SSE2__
+    std::pair<int8_t, ls::sse_tag>, std::pair<int16_t, ls::sse_tag>,
+    std::pair<int32_t, ls::sse_tag>, std::pair<int64_t, ls::sse_tag>,
+    std::pair<float, ls::sse_tag>, std::pair<double, ls::sse_tag>
+#ifdef __AVX2__
+    , std::pair<int8_t, ls::avx_tag>, std::pair<int16_t, ls::avx_tag>,
+    std::pair<int32_t, ls::avx_tag>, std::pair<int64_t, ls::avx_tag>,
+    std::pair<float, ls::avx_tag>, std::pair<double, ls::avx_tag>
+#endif //__AVX2__
+#endif //__SSE2__
+>;
+TYPED_TEST_CASE(SimdTypes, TestTypes);
+
+#ifdef __SSE2__
+TYPED_TEST(SimdTypes, AddTypedTest)
+{
+    using type = typename TypeParam::first_type;
+    using tag = typename TypeParam::second_type;
+    using simd = typename ls::traits< type, tag >::simd_type;
+    constexpr size_t size = ls::traits< type, tag >::simd_size;
+
+    simd a = ls::from_value< type, tag >( 1 );
+    simd b = ls::from_value< type, tag >( 2 );
+    simd c = ls::add< type, tag >( a, b );
+
+    type* pCmp = reinterpret_cast<type*>( &c );
+    for( size_t i = 0; i < size; ++i )
+    {
+        EXPECT_EQ( 3, pCmp[ i ] );
+    }
+}
+#endif //__SSE2__
