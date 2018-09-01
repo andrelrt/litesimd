1b9b4357f5baebe6905bde163c30ed6d0367c5a2
André Tupinambá <andrelrt@gmail.com>
2018-09-02 00:00:35 +0100

    Introduce algorithm tests

---
+++ test/algorithm.cpp
@@ -0,0 +1,90 @@
+// MIT License
+//
+// Copyright (c) 2018 André Tupinambá
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <litesimd/types.h>
+#include <litesimd/algorithm.h>
+#include "gtest/gtest.h"
+
+namespace ls = litesimd;
+
+template <typename T> class AlgorithmTypedTest: public ::testing::Test {};
+
+using TestTypes = ::testing::Types<
+#ifdef __SSE2__
+    std::pair<int8_t, ls::sse_tag>, std::pair<int16_t, ls::sse_tag>,
+    std::pair<int32_t, ls::sse_tag>, std::pair<int64_t, ls::sse_tag>
+#ifdef __AVX2__
+    , std::pair<int8_t, ls::avx_tag>, std::pair<int16_t, ls::avx_tag>,
+    std::pair<int32_t, ls::avx_tag>, std::pair<int64_t, ls::avx_tag>
+#endif //__AVX2__
+#endif //__SSE2__
+>;
+TYPED_TEST_CASE(AlgorithmTypedTest, TestTypes);
+
+#ifdef __SSE2__
+TYPED_TEST(AlgorithmTypedTest, ForEachTypedTest)
+{
+    using type = typename TypeParam::first_type;
+    using tag = typename TypeParam::second_type;
+    using simd = ls::simd_type< type, tag >;
+
+    ls::for_each( simd( 3 ), []( int index, type val )
+    {
+        EXPECT_EQ( 3, val ) << "Error on index " << index;
+        return true;
+    } );
+
+    ls::for_each( ls::iota< type, tag >( 0 ), []( int index, type val )
+    {
+        EXPECT_EQ( static_cast<type>( index ), val ) << "Error on index " << index;
+        return true;
+    } );
+
+    int expected = simd::simd_size;
+    ls::for_each_backward( ls::iota< type, tag >( 1 ), [&expected]( int index, type val )
+    {
+        EXPECT_EQ( static_cast<type>( expected ), val ) << "Error on index " << index;
+        --expected;
+        return true;
+    } );
+}
+
+TYPED_TEST(AlgorithmTypedTest, IotaTypedTest)
+{
+    using type = typename TypeParam::first_type;
+    using tag = typename TypeParam::second_type;
+    using simd = ls::simd_type< type, tag >;
+
+    ls::for_each( ls::iota< type, tag >( 0 ), []( int index, type val )
+    {
+        EXPECT_EQ( static_cast<type>( index ), val ) << "Error on index " << index;
+        return true;
+    } );
+
+    ls::for_each( ls::iota< type, tag >( 10 ), []( int index, type val )
+    {
+        EXPECT_EQ( static_cast<type>( index + 10 ), val ) << "Error on index " << index;
+        return true;
+    } );
+}
+
+#endif //__SSE2__
