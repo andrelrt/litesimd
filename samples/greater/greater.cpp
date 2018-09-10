8b018c893c27129d5a33e196fe5b615d4cb08427
André Tupinambá <andrelrt@gmail.com>
2018-09-10 13:09:22 +0100

    Making SSE only compatible

--- samples/greater/greater.cpp
+++ samples/greater/greater.cpp
@@ -32,15 +32,20 @@ int main()
 {
     namespace ls = litesimd;
 
-    // int32_t vector in default instruction set (AVX2)
+    // int32_t vector in default instruction set (SSE/AVX)
     ls::t_int32_simd cmp;
 
     // litesimd types are intrincs compatible
+#ifdef LITESIMD_HAS_AVX
     cmp = _mm256_set_epi32( 80, 70, 60, 50, 40, 30, 20, 10 );
+#else
+    // No AVX, using SSE
+    cmp = _mm_set_epi32( 40, 30, 20, 10 );
+#endif // LITESIMD_HAS_AVX
 
     int32_t val = 5;
 
-    // int32_simd_size is how many int32_t fits on t_int32_simd (8)
+    // int32_simd_size is how many int32_t fits on t_int32_simd (4 - SSE, 8 - AVX)
     for( size_t i = 0; i <= ls::int32_simd_size; ++i )
     {
         // Compare 'val' against all 'cmp' values
@@ -82,3 +87,16 @@ int main()
     }
     return 0;
 }
+
+// The output on AVX will be
+//
+// $ ./greater
+// The value 5 is less than all values of (80, 70, 60, 50, 40, 30, 20, 10)
+// The value 15 is between items 0 and 1 of (80, 70, 60, 50, 40, 30, 20, 10)
+// The value 25 is between items 1 and 2 of (80, 70, 60, 50, 40, 30, 20, 10)
+// The value 35 is between items 2 and 3 of (80, 70, 60, 50, 40, 30, 20, 10)
+// The value 45 is between items 3 and 4 of (80, 70, 60, 50, 40, 30, 20, 10)
+// The value 55 is between items 4 and 5 of (80, 70, 60, 50, 40, 30, 20, 10)
+// The value 65 is between items 5 and 6 of (80, 70, 60, 50, 40, 30, 20, 10)
+// The value 75 is between items 6 and 7 of (80, 70, 60, 50, 40, 30, 20, 10)
+// The value 85 is greater than all values of (80, 70, 60, 50, 40, 30, 20, 10)
