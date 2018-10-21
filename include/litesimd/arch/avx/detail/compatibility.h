da50784096dbd2935f2cfdd577e3978d8ce60073
André Tupinambá <andrelrt@gmail.com>
2018-10-22 00:13:05 +0100

    Another try to fix MSVC compilation

--- include/litesimd/arch/avx/detail/compatibility.h
+++ include/litesimd/arch/avx/detail/compatibility.h
@@ -32,7 +32,7 @@ static inline double _mm256_cvtsd_f64( __m256d vec ) { return _mm_cvtsd_f64( _mm
 #endif
 
 #if defined(_MSC_VER)
-static inline __m256i _mm256_insert_epi64( __m256i vec, int64_t val, constexpr int imm8 )
+static constexpr __m256i _mm256_insert_epi64( __m256i vec, int64_t val, const int imm8 )
 {
     constexpr int mask = 3 << (2*imm8);
     return _mm256_blend_epi32( vec, _mm256_set1_epi64x( val ), mask );
